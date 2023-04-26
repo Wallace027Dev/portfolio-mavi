6cec2343686aec490386c3f0f3c56b4d9acf2362
Wallace Vieira <wallaceofc@hotmail.com>
2023-04-26 19:56:51 -0300

    feat: 'Adicionando nav bar'

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,15 +1,44 @@
+import styled from "styled-components";
 
+const Navigator = styled.nav`
+  div {
+    margin-top: 6rem;
+  }
+
+  .navigation {
+    position: absolute;
+    left: 32px;
+    top: 0;
+    a {
+      display: inline-block;
+      margin-right: 10px;
+      text-decoration: none;
+      color: var(--font);
+			font-size: 2rem;
+    }
+  }
+
+  .user-title {
+    position: absolute;
+    top: 2%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+		font-size: 4rem;
+  }
+`;
 
 function Nav() {
   return (
     <>
-      <nav>
-				<div>
-					<a href="/">WORK</a>
-					<a href="/">CONTACT</a>
-				</div>
-				<h1>MARIA VITORIA ALCANTARA FRANÇA</h1>
-			</nav>
+      <Navigator>
+        <div className="navigation">
+          <a href="/">WORK</a>
+          <a href="/">CONTACT</a>
+        </div>
+        <div className="user-title">
+          <h1>MARIA VITORIA ALCANTARA FRANÇA</h1>
+        </div>
+      </Navigator>
     </>
   );
 }
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,8 +1,13 @@
+import styled from "styled-components";
+
+const Main = styled.main`
+  margin-top: 150px;
+`;
 function Home() {
   return (
-    <>
+    <Main>
       <h1>Olá Mundo</h1>
-    </>
+    </Main>
   );
 }
 
