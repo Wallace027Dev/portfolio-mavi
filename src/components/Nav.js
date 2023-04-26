0938be3a997a154d58474e1d365875f6f21a5763
Wallace Vieira <wallaceofc@hotmail.com>
2023-04-26 19:07:10 -0300

    First commit 🔥

---
+++ src/components/Nav.js
@@ -0,0 +1,9 @@
+function Nav() {
+  return (
+    <>
+      <h1>Aqui está a Nav</h1>
+    </>
+  );
+}
+
+export default Nav;
---
+++ src/pages/Home.js
@@ -0,0 +1,9 @@
+function Home() {
+  return (
+    <>
+      <h1>Olá Mundo</h1>
+    </>
+  );
+}
+
+export default Home;
