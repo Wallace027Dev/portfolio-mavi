0de8efbd788ff35518387ae918b3c3929b67962a
Wallace Vieira <wallaceofc@hotmail.com>
2023-05-12 20:42:55 -0300

    feat: "sending amendments of the 12/5th, requested by Maria"

--- src/pages/Arts.js
+++ src/pages/Arts.js
@@ -2,11 +2,15 @@ import styled from "styled-components";
 
 import bairro from "../assets/bairro.jpeg";
 import panificadora from "../assets/panificadora.jpeg";
+import henrry from "../assets/henrry.jpeg";
+import romeu from "../assets/romeu.jpeg";
+import maviBlue from "../assets/mavi-blue.jpeg";
+import maviOrange from "../assets/mavi-orange.jpeg";
 import ImageLeft from "../components/ImageLeft";
 import ImageRight from "../components/ImageRight";
 
 const Main = styled.section`
-  margin-top: 18rem;
+  margin-top: 8rem;
 
   /* Para telas menores que 1440px (tablets e celulares) */
   @media screen and (max-width: 1440px) {
@@ -31,9 +35,8 @@ function Arts() {
           date="mai/2022"
           measure="falta (cm)"
         />
-
         <ImageRight
-          foto={bairro}
+          foto={henrry}
           alt="Pintura de Henrry"
           material="Tinta Acrílica e Marcador"
           date="mar/2023"
@@ -46,9 +49,36 @@ function Arts() {
           date="mar/2022"
           measure="falta (cm)"
         />
-
         <ImageRight
           foto={panificadora}
+          alt="Pintura Vista da Janela"
+          material="Tinta Acrílica e Marcador"
+          date="fev/2022"
+          measure="falta (cm)"
+        />
+        <ImageLeft
+          foto={maviBlue}
+          alt="Pintura Tênis no Fio"
+          material="Tinta Acrílica e Marcador"
+          date="mar/2022"
+          measure="falta (cm)"
+        />
+        <ImageRight
+          foto={maviOrange}
+          alt="Pintura Vista da Janela"
+          material="Tinta Acrílica e Marcador"
+          date="fev/2022"
+          measure="falta (cm)"
+        />
+        <ImageLeft
+          foto={romeu}
+          alt="Pintura Tênis no Fio"
+          material="Tinta Acrílica e Marcador"
+          date="mar/2022"
+          measure="falta (cm)"
+        />
+        <ImageRight
+          foto={maviOrange}
           alt="Pintura Vista da Janela"
           material="Tinta Acrílica e Marcador"
           date="fev/2022"
